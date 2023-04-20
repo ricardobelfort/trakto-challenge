e5a4877761d859d2ed4d6d39754f424881f3c138
Ricardo Belfort <rbelfort2004@gmail.com>
2023-04-20 13:27:28 -0400

    adding navbar component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,17 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { SharedModule } from '../shared/shared.module';
 import { CoreRoutingModule } from './core-routing.module';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-
 @NgModule({
-  declarations: [
-    DashboardComponent
-  ],
-  imports: [
-    CommonModule,
-    CoreRoutingModule
-  ]
+  declarations: [DashboardComponent],
+  imports: [CommonModule, CoreRoutingModule, SharedModule],
 })
-export class CoreModule { }
+export class CoreModule {}
